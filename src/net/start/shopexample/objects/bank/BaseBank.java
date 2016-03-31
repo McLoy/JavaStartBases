798a644b2090512787ccbd7c581caabaec8c8cb3
McLoy <tkachenko.vlad@hotmail.com>
2016-03-31 14:56:03 +0300

    update

---
+++ src/net/start/shopexample/objects/bank/BaseBank.java
@@ -0,0 +1,13 @@
+package net.start.shopexample.objects.bank;
+
+/**
+ * Created by McLoy on 31.03.2016.
+ */
+public class BaseBank {
+    String name, creditDescription;
+
+    public void chekInfo(){}
+
+    public void giveCredit(){}
+
+}
---
+++ src/net/start/shopexample/objects/bank/PrivatBank.java
@@ -0,0 +1,7 @@
+package net.start.shopexample.objects.bank;
+
+/**
+ * Created by McLoy on 31.03.2016.
+ */
+public class PrivatBank extends BaseBank {
+}
