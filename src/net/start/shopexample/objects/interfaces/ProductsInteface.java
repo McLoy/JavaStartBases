3886c35417b77ae55696ec29b419f435f902b0ef
McLoy <tkachenko.vlad@hotmail.com>
2016-03-31 20:09:53 +0300

    update

--- src/net/start/shopexample/objects/interfaces/ProductsInteface.java
+++ src/net/start/shopexample/objects/interfaces/ProductsInteface.java
@@ -4,4 +4,15 @@ package net.start.shopexample.objects.interfaces;
  * Created by McLoy on 31.03.2016.
  */
 public interface ProductsInteface {
+
+    double getPrice();
+
+    boolean hasGuarantee();
+
+    String getName();
+
+    DepartmentInterface getDepartment();
+
+    String getCompany();
+
 }
