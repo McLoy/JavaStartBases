0d17eb90a9cc2575d20a0dab1a5947b2622e2c21
McLoy <tkachenko.vlad@hotmail.com>
2016-04-04 20:36:00 +0300

    update

--- src/net/start/shopexample/objects/Main.java
+++ src/net/start/shopexample/objects/Main.java
@@ -7,6 +7,7 @@ import net.start.shopexample.objects.department.PCDepartment;
 import net.start.shopexample.objects.department.TVDepartment;
 import net.start.shopexample.objects.interfaces.DepartmentInterface;
 import net.start.shopexample.objects.interfaces.ProductsInteface;
+import net.start.shopexample.objects.product.BaseProduct;
 import net.start.shopexample.objects.product.Computer;
 import net.start.shopexample.objects.product.Subwoofer;
 import net.start.shopexample.objects.product.TV;
@@ -38,39 +39,66 @@ public class Main {
 //        tv.selectChannel(5);
 //        tv.selectChannel("Sports");
 
-        ArrayList<DepartmentInterface>  deps = new ArrayList<DepartmentInterface>();
-        PCDepartment pcDep = new PCDepartment();
-        pcDep.setName("Отдел компьютеров");
-        deps.add(pcDep);
-        TVDepartment tvDep = new TVDepartment();
-        tvDep.setName("Отдел телевизоров");
-        deps.add(tvDep);
+
 
         ArrayList<ProductsInteface>  goods = new ArrayList<ProductsInteface>();
-        for (int i = 0; i < 3; i++) {
-            Computer PC = new Computer("Comp " + Integer.toString(i+1));
+        for (int i = 1; i <= 10; i++) {
+            Computer PC = new Computer("Comp " + Integer.toString(i));
             goods.add(PC);
         }
 
+        ArrayList<ProductsInteface>  goodstv = new ArrayList<ProductsInteface>();
+
+        TV tv = new TV("TV Samsung");
+        goodstv.add(tv);
+
+        ArrayList<DepartmentInterface>  deps = new ArrayList<DepartmentInterface>();
+        PCDepartment pcDep = new PCDepartment();
+        pcDep.setName("Отдел компьютеров");
+        pcDep.setProductList(goods);
+        deps.add(pcDep);
+        TVDepartment tvDep = new TVDepartment();
+        tvDep.setName("Отдел телевизоров");
+        tvDep.setProductList(goodstv);
+        deps.add(tvDep);
+
         Administrator admin = new Administrator("Матвей");
+
+
 
         SellerHall sh = new SellerHall(deps, "Торговый зал", goods, admin);
         System.out.println("Created shop with hall " + sh.getName());
         System.out.println("Administrator for today: " + sh.getAdministrator().getName());
-        if (!goods.isEmpty()){
-        System.out.println("Such products are here: ");
-        for (int k = 0; k < goods.size(); k++)
-            System.out.println(goods.get(k).getName());
-        } else {
-            System.out.println("There are no products! Sorry!");
-        }
-//        System.out.println("Such departments are available: " + sh.getDepartmentList());
+
         if (!deps.isEmpty()){
             System.out.println("Such departments are available: ");
             for (int k = 0; k < deps.size(); k++)
                 System.out.println(deps.get(k).getName());
         } else {
             System.out.println("There are no departments!");
+        }
+
+
+        if (!goods.isEmpty()){
+        System.out.println("Such products are here: ");
+        for (int z = 0; z < goods.size(); z++) {
+            for (int i = 0; i < deps.size(); i++){
+                if (deps.get(i).getProductList().contains(goods.get(z))) {
+                    System.out.println("Product: " + goods.get(z).getName() + ", Department: " + deps.get(i).getName());
+                }
+            }
+        }
+
+            for (int z = 0; z < goodstv.size(); z++) {
+                for (int i = 0; i < deps.size(); i++){
+                    if (deps.get(i).getProductList().contains(goodstv.get(z))) {
+                        System.out.println("Product: " + goodstv.get(z).getName() + ", Department: " + deps.get(i).getName());
+                    }
+                }
+            }
+
+        }else {
+            System.out.println("There are no products! Sorry!");
         }
 
         System.out.println("Have a nice day!");
